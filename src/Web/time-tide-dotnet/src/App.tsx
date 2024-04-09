a598d3aa65e3df81f39d9421c63b801881381d30
emidio@morgia.info <emidio@morgia.info>
2024-04-09 23:09:03 +0200

    web

--- src/Web/time-tide-dotnet/src/App.tsx
+++ src/Web/time-tide-dotnet/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Time Tide Dotnet ok
+          Time Tide Dotnet
         </p>
        
       </header>
